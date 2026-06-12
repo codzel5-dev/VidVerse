ee6c1a675f06692a7c364191afde2bcb9c0b2fa2
Z User <z@container>
2026-06-12 17:40:29 +0000

    07695b17-32a4-4ae1-8ba2-72de53f8a2fc

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  allowedDevOrigins: ['127.0.0.1'],
 };
 
 export default nextConfig;
